9058fd59e912c17fdf496ff0382e44735e6b6911
Subhali AR Otti <167698374+subhaliAR21@users.noreply.github.com>
2025-10-06 18:40:59 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,28 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // NEW UPDATED CODE
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+document.body.style.margin = 0;
+document.body.style.backgroundColor = '#f3f4f6';
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
